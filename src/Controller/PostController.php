152b65a3cac99b3d9e6380d782db8d411cc2ffb1
Ouada Mohamed <124060190+moha95370@users.noreply.github.com>
2023-09-12 21:02:44 +0200

    ajout contoller

---
+++ src/Controller/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'controller_name' => 'PostController',
+        ]);
+    }
+
+    #[Route('/post/{$id}', name: 'post_view')]
+    public function post_view(): Response
+    {
+        return $this->render('post/post.html.twig', [
+            'controller_name' => 'PostController',
+        ]);
+    }
+}
